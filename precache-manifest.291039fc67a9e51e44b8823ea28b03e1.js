161bcdc17ca752cce84cb068094b8ac7265ff530
kdev <kdev.software1110@gmail.com>
2019-11-12 14:14:04 -0500

    Updates

---
+++ precache-manifest.291039fc67a9e51e44b8823ea28b03e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0937f99e4a5efbe52fa81a2bb682d98b",
+    "url": "/k-theme004-react/index.html"
+  },
+  {
+    "revision": "3edb7628e4187677ee11",
+    "url": "/k-theme004-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f895d45963f4b291b2cb",
+    "url": "/k-theme004-react/static/js/2.266973cc.chunk.js"
+  },
+  {
+    "revision": "3edb7628e4187677ee11",
+    "url": "/k-theme004-react/static/js/main.42d09a00.chunk.js"
+  },
+  {
+    "revision": "6dedbc0e5dc3ffeb996b",
+    "url": "/k-theme004-react/static/js/runtime-main.f0dadcf4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/k-theme004-react/static/media/logo.25bf045c.svg"
+  }
+]);
